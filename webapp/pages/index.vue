71f7c98939ace31e5ad7fe0a28eaa8f9a71c5d42
Dirk Arends <dirk@arends.com.au>
2023-11-26 13:18:07 +1100

    Fill homepage with quick links

--- webapp/pages/index.vue
+++ webapp/pages/index.vue
@@ -12,6 +12,43 @@ definePageMeta({
 
 <template>
   <div>
-    <h2>Hello</h2>
+    <div class="hero">
+      <p>Moonee Valley District Scouts presents</p>
+      <h2>Capture the Flag: A game of stealth of tactics</h2>
+    </div>
+
+    <h3>View the teams playing:</h3>
+    <ul>
+      <li><NuxtLink :to="`/teams`">Team list</NuxtLink></li>
+    </ul>
+
+    <h3>Locate the current locaation of the Flags:</h3>
+    <ul>
+      <li><NuxtLink :to="`/trackers`">Tracker list</NuxtLink></li>
+    </ul>
+
+    <h3>Browse the raw log data:</h3>
+    <ul>
+      <li>
+        <NuxtLink :to="`/flags`">
+          Flag list (in {{ $config.public.flagWindowIntervalMinutes }} minute
+          windows)
+        </NuxtLink>
+      </li>
+      <li><NuxtLink :to="`/logs`">Log list</NuxtLink></li>
+    </ul>
   </div>
 </template>
+
+<style scoped>
+.hero {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.hero h2 {
+  background-color: #1d483a; /*MVD dark green*/
+  box-shadow: 0 0 11px 10px #1d483a; /*MVD dark green*/
+  border-radius: 40px;
+}
+</style>
