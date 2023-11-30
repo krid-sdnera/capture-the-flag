5394ead73ad46477e7a6ac26eb6c5aeda010dc6a
Dirk Arends <dirk@arends.com.au>
2023-11-30 17:41:03 +1100

    Disable SSR
    Was causing issues with useFetch not making the request and reporting the data was null

--- webapp/nuxt.config.ts
+++ webapp/nuxt.config.ts
@@ -2,6 +2,7 @@
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   modules: ["nuxt3-socket.io"],
   build: { transpile: ["@fawmi/vue-google-maps"] },
